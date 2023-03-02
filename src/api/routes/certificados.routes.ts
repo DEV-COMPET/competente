31372fd41425750f349a5459374f728024a94bad
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2023-03-02 12:10:02 -0300

    Creating the route to list all certificates

--- src/api/routes/certificados.routes.ts
+++ src/api/routes/certificados.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import createCertificadosController from "../modules/certificados/useCases/createCertificados"
+import getCertificatesController from "../modules/certificados/useCases/getCertificados";
 const certificadosRouter = Router();
-certificadosRouter.post("/",async(request,response)=>createCertificadosController().handle(request,response));
+certificadosRouter.post("/", async (request, response) => createCertificadosController().handle(request, response));
+certificadosRouter.get("/", async (request, response) => getCertificatesController().handle(request, response))
 export default certificadosRouter;
--- src/api/server.ts
+++ src/api/server.ts
@@ -2,9 +2,11 @@ import bodyParser from "body-parser";
 import express from "express";
 import competianosRouter from "./routes/competianos.routes";
 import "../database"
+import certificadosRouter from "./routes/certificados.routes";
 const port = process.env.PORT || 4444
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/competianos", competianosRouter)
+app.use("/certificados",certificadosRouter)
 app.listen(4444, () => console.log(`server listening on port ${port}`))
