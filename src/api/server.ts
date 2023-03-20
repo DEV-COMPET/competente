10a27d7db1fa0c94af640aa4ae5c6d0119e5c27d
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2023-03-20 16:50:34 -0300

    Creating a route to receive webhooks from autentique

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,12 +1,14 @@
 import bodyParser from "body-parser";
 import express from "express";
 import competianosRouter from "./routes/competianos.routes";
-import "../database"
 import certificadosRouter from "./routes/certificados.routes";
+import webhooksRouter from "./routes/webhooks.routes";
+import "../database"
 const port = process.env.PORT || 4444
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/competianos", competianosRouter)
-app.use("/certificados",certificadosRouter)
+app.use("/certificados", certificadosRouter)
+app.use("/webhooks", webhooksRouter)
 app.listen(4444, () => console.log(`server listening on port ${port}`))
