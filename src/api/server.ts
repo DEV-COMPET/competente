337266a5db8bed67f1f92d05c31adf8a4d0d8dfd
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2023-08-28 13:32:05 -0300

    Chore(webhook route): adicionando rota de webhook ao server

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,15 +1,16 @@
-import { competianosRoutes } from "./routes/competianos.routes";
-import "../database"
 import fastify from "fastify";
+import { competianosRoutes } from "./routes/competianos.routes";
 import { certificadosRoutes } from "./routes/certificados.routes";
+import { webhooksRoutes } from "./routes/webhooks.routes";
+import { env } from "@/env";
+import "../database"
 
-const port = (process.env.PORT || 4444) as number
+const port = env.PORT
 const app = fastify();
 
 app.register(competianosRoutes, { prefix: 'competianos' })
 app.register(certificadosRoutes, { prefix: 'certificados' })
-// app.register(certificadosRouter, { prefix: 'certificados' })
-// app.register(webhooksRouter, { prefix: 'webhooks' })
+app.register(webhooksRoutes, { prefix: 'webhooks' })
 
 app.listen({
 	host: '0.0.0.0', // auxilia front-end a conectar com aplicação mais pra frente
@@ -17,10 +18,3 @@ app.listen({
 }).then(() => {
 	console.log(`server listening on port ${port}`);
 });
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use("/competianos", competianosRouter)
-// app.use("/certificados", certificadosRouter)
-// app.use("/webhooks", webhooksRouter)
-// app.listen(4444, () => console.log(`server listening on port ${port}`))
