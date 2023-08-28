d0b7d44ef8a247a1546beb825370901e8cafa653
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2023-08-28 13:31:02 -0300

    Continuação do anterior: remoção dos usecases da implementação anterior

--- src/api/modules/webhooks/useCases/createDiscordWebhook/index.ts
+++
@@ -1,8 +0,0 @@
-import { CreateDiscordWebhookUseCase } from "./createDiscordWebhookUseCase";
-import { CreateDiscordWebhookController } from "./createDiscordWebhookController";
-import { WebhookRepository } from "../../repositories/defaultMongoDBRepository/webhooksRepository";
-export default (): CreateDiscordWebhookController => {
-  const repository = new WebhookRepository();
-  const useCase = new CreateDiscordWebhookUseCase(repository);
-  return new CreateDiscordWebhookController(useCase);
-};
--- src/api/modules/webhooks/useCases/getWebhookByGuild/index.ts
+++
@@ -1,8 +0,0 @@
-import { GetWebhookByGuildUseCase } from "./getWebhookByGuildUseCase";
-import { GetWebhookByGuildController } from "./getWebhookByGuildController";
-import { WebhookRepository } from "../../repositories/defaultMongoDBRepository/webhooksRepository";
-export default (): GetWebhookByGuildController => {
-  const repository = new WebhookRepository();
-  const useCase = new GetWebhookByGuildUseCase(repository);
-  return new GetWebhookByGuildController(useCase);
-};
