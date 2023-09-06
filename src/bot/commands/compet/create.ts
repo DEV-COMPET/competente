b63f94ee42629ec894c2c12b34750257a0b0ddff
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2023-09-06 14:17:31 -0300

    Feat(factorie): factorie de criaão de embed de não ser admin no create

--- src/bot/commands/compet/create.ts
+++ src/bot/commands/compet/create.ts
@@ -1,10 +1,17 @@
 import { modal } from "@/bot/modals/compet/createMemberModal/createMemberModal";
 import { Command } from "../../structures/Command";
+import { makeNotAdminEmbed } from "@/bot/utils/embed/makeNotAdminEmbed";
 
 export default new Command({
   name: "create",
   description: "Esse comando adiciona um novo competiano ao compet",
   run: async ({ interaction }) => {
+    const member = await interaction.guild?.members.fetch(interaction.user.id);
+    const isADM = member?.permissions.has("Administrator");
+
+    if (!isADM)
+      return makeNotAdminEmbed(interaction)
+
     await interaction.showModal(modal);
   },
 });
