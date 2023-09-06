2534d30b144599910349d49248b6ecad7ac1d4e1
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2023-09-06 14:15:03 -0300

    Feat(factorie): factorie de criaão de embed de não ser admin

---
+++ src/bot/utils/embed/makeNotAdminEmbed.ts
@@ -0,0 +1,27 @@
+import { ExtendedInteraction } from "@/bot/typings/Commands";
+import { EmbedBuilder } from "discord.js";
+
+export async function makeNotAdminEmbed(interaction: ExtendedInteraction) {
+    const embed = new EmbedBuilder()
+        .setColor(0xf56565)
+        .setTitle("Não foi possível utilizar este comando!")
+        .setDescription("Você não possui autorização necessária.")
+        .setThumbnail(
+            "https://www.pngfind.com/pngs/m/0-1420_red-cross-mark-clipart-green-checkmark-red-x.png"
+        )
+        .addFields(
+            { name: "Código do erro", value: "401", inline: false },
+            {
+                name: "Mensagem do erro",
+                value:
+                    "Você precisa ter permissão de administrador para executar esse comando",
+                inline: false,
+            }
+        );
+
+    return await interaction.reply({
+        content: "Não foi possível executar este comando",
+        ephemeral: true,
+        embeds: [embed],
+    });
+}
