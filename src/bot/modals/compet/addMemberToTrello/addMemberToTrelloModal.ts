0bd8b36dabdb98dd125ebf5c72b30ae3209ab023
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2024-02-26 12:29:31 -0300

    handle errors

--- src/bot/modals/compet/addMemberToTrello/addMemberToTrelloModal.ts
+++ src/bot/modals/compet/addMemberToTrello/addMemberToTrelloModal.ts
@@ -6,6 +6,7 @@ import { extractInputData } from "./utils/extractInputData";
 import { validateInputData } from "./utils/validateInputData";
 import { editErrorReply } from "@/bot/utils/discord/editErrorReply";
 import { editSucessReply } from "@/bot/utils/discord/editSucessReply";
+import fetch from "node-fetch";
 
 const { inputFields, modalBuilderRequest }: {
     inputFields: TextInputComponentData[];
@@ -31,19 +32,50 @@ export default new Modal({
             return await editErrorReply({
                 error: validateInputDataResponse.value.error, interaction,
                 title: "Não foi possível adicionar o competiano ao Trello"
-            })
+            });
         } 
 
-        return await editSucessReply({
-            interaction, title: "Competiano adicionado ao Trello!",
-            fields: [
-                {
-                    name: "E-mail",
-                    value: email,
-                    inline: false
-                },
-            ],
+        const bodyData = `{
+        "fullName": "<string>"
+        }`;
+
+        fetch(`https://api.trello.com/1/boards/EHISYWtc/members?key=9fbd93571f3419b52bb337324d0fb72f&token=ATTA565430860ae464d902f57b17c96a2737f6b79ac33077f63b147ee6ab67e828253796F1BB&email=${email}`,{
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: bodyData
         })
+        .then(response => {
+            console.log(
+            `Response: ${response.status} ${response.statusText}`
+            );
+
+            if(response.status == 200)
+                return response.text();
+            else throw new Error(response.statusText);
+        })
+        .then(text => {
+            console.log(text);
+            return editSucessReply({
+                interaction, title: "Competiano adicionado ao Trello!",
+                fields: [
+                    {
+                        name: "E-mail",
+                        value: email,
+                        inline: false
+                    },
+                ],
+            });
+        })
+        .catch(err => {
+            return editErrorReply({
+                error: err, interaction,
+                title: "Não foi possível adicionar o competiano ao Trello"
+            });
+        });
+
+        
     },
 });
 
