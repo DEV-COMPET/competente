2dc55d111a9086bb7f2c375ad9a94e221cee86c9
Arthur <dss.arthur0@gmail.com>
2024-04-15 16:30:48 -0300

    Make sucess embed if competiano got a new role OR a error reply if competiano dont get the role

--- src/bot/selectMenus/addToCompet/selectTeam/selectTeamMenu.ts
+++ src/bot/selectMenus/addToCompet/selectTeam/selectTeamMenu.ts
@@ -1,14 +1,36 @@
 import { SelectMenu } from "@/bot/structures/SelectMenu";
 import { customId} from "@/bot/selectMenus/addToCompet/selectTeam/selectTeamMenuData.json";
+import { competianoChosen } from "../variable/competianoChosen";
+import { giveMemberRole } from "../utils/giveMemberRole";
+import { editErrorReply } from "@/bot/utils/discord/editErrorReply";
+import { makeSuccessEmbed } from "@/bot/utils/embed/makeSuccessEmbed";
 
 
 export default new SelectMenu ({
     customId,
 
     run: async ({ interaction }) => {
+
+        await interaction.deferReply({ ephemeral: true });
         
         const roleName = interaction.values[0];
 
-        console.log(roleName);
+        const giveMemberRoleReponse = await giveMemberRole({ interaction, roleName, competiano: competianoChosen[competianoChosen.length - 1] });
+        if (giveMemberRoleReponse.isLeft()) {
+            return await editErrorReply({
+                interaction,
+                title: "Não foi possível adicionar o cargo ao competiano",
+                error: giveMemberRoleReponse.value.error
+            });
+        };
+
+        await interaction.editReply({
+            embeds: [
+                makeSuccessEmbed({
+                    title: "Cargo adicionado com sucesso",
+                    interaction
+                })
+            ]
+        });
     }
 })
