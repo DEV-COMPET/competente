c1fc4a069ee603b87ba97335b6afe7a33c08beaa
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2024-05-10 14:51:01 -0300

    REmovendo webhook autentique

--- src/bot/events/ready.ts
+++ src/bot/events/ready.ts
@@ -4,29 +4,29 @@ import { Event } from "../structures/Event";
 import { env } from "@/env";
 
 export default new Event("ready", "once", async function () {
-  let webhook = client.webhook;
-  if (!webhook) {
-    await client.createWebhook({
-      channelName: "geral",
-      webhookName: "Autentique Webhook",
-    });
-    webhook = client.webhook;
-  }
-  if (!webhook) return;
-  try {
-    const url = env.ENVIRONMENT === "development" ? "http://localhost:4444" : env.HOST;
-    const response = await fetch(`${url}/webhooks/discord/`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        name: webhook.name,
-        id: webhook.id,
-        token: webhook.token,
-        guildId: webhook.guildId,
-      }),
-    });
-    const data = await response.json();
-  } catch (error) {
-    console.error(error);
-  }
+  // let webhook = client.webhook;
+  // if (!webhook) {
+  //   await client.createWebhook({
+  //     channelName: "geral",
+  //     webhookName: "Autentique Webhook",
+  //   });
+  //   webhook = client.webhook;
+  // }
+  // if (!webhook) return;
+  // try {
+  //   const url = env.ENVIRONMENT === "development" ? "http://localhost:4444" : env.HOST;
+  //   const response = await fetch(`${url}/webhooks/discord/`, {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       name: webhook.name,
+  //       id: webhook.id,
+  //       token: webhook.token,
+  //       guildId: webhook.guildId,
+  //     }),
+  //   });
+  //   const data = await response.json();
+  // } catch (error) {
+  //   console.error(error);
+  // }
 });
